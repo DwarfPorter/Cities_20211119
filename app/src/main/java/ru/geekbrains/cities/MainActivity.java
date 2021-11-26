c09f9fcbd1229f80e5c931583cfe91359d0b4fde
DwarfPorter <mrvlmor@gmail.com>
2021-11-26 20:23:55 +0300

    Обработка пунктов меню

--- app/src/main/java/ru/geekbrains/cities/MainActivity.java
+++ app/src/main/java/ru/geekbrains/cities/MainActivity.java
@@ -1,10 +1,12 @@
 package ru.geekbrains.cities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +28,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.action_about:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("")
+                        .replace(R.id.fragment_container, new AboutFragment())
+                        .commit();
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
